ff4745099d8bcc83d4123c3fd62893c49c776071
Bruno Barreto <brunocbarreto2012@gmail.com>
2021-02-04 06:40:12 -0300

    .

---
+++ AspNetCore.Api/BasicCrud.Api/Extensions/BasicCrudApiDbContextExtencion.cs
@@ -0,0 +1,22 @@
+﻿using BasicCrud.Api.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicCrud.Api.Extensions
+{
+    public static class BasicCrudApiDbContextExtencion
+    {
+        public static void DefineDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BasicCrudApiDbContext>(
+                options =>
+                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+             ); ;
+        }
+    }
+}
---
+++ AspNetCore.Api/BasicCrud.Api/Model/BasicCrudApiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicCrud.Api.Model
+{
+    public class BasicCrudApiDbContext : DbContext
+    {
+        public BasicCrudApiDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
